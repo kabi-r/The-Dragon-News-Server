a6e67d88cb6c9d50b8775be22beafaf1cb50cac9
kabi-r <programmerkabirr@gmail.com>
2023-04-29 16:03:24 +0600

    back server api send

--- index.js
+++ index.js
@@ -1,18 +1,37 @@
-const express = require('express')
-const app = express()
-const cors = require('cors')
+const express = require("express");
+const app = express();
+const cors = require("cors");
 const port = process.env.PORT || 5000;
-const categories = require ('./data/categories.json')
+const categories = require("./data/categories.json");
+const news = require("./data/News.json");
 
-app.use(cors())
+app.use(cors());
 // respond with "hello world" when a GET request is made to the homepage
-app.get('/', (req, res) => {
-  res.send('dsa')
-})
+app.get("/", (req, res) => {
+  res.send("Dragon Server is Coming");
+});
 
-app.get('/categories', (req, res) => {
-  res.send(categories)
-})
-app.listen(port,()=>{
-    console.log(`Dragon `);
-})
+app.get("/categories", (req, res) => {
+  res.send(categories);
+});
+app.get("/news", (req, res) => {
+  res.send(news);
+});
+app.get("/news/:id", (req, res) => {
+  const id = req.params.id;
+  const selectedNews = news.find((n) => n._id === id);
+  res.send(selectedNews);
+});
+
+app.get("/categories/:id", (req, res) => {
+  const id = req.params.id;
+  if (id == 0) {
+    res.send(news);
+  } else {
+    const categoryNews = news.filter((n) => n.category_id == id);
+    res.send(categoryNews);
+  }
+});
+app.listen(port, () => {
+  console.log(`Dragon `);
+});
